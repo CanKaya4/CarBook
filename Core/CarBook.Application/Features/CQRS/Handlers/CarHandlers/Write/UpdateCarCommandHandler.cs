791c2ee5748819af5bd1292f11d06312623d5465
Ali <alikaya4275@gmail.com>
2025-11-12 16:23:40 +0300

    Car CQRS işlemleri

---
+++ Core/CarBook.Application/Features/CQRS/Handlers/CarHandlers/Write/UpdateCarCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using CarBook.Application.Features.CQRS.Commands.CarCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CarBook.Application.Features.CQRS.Handlers.CarHandlers.Write
+{
+    public class UpdateCarCommandHandler
+    {
+        private readonly IRepository<Car> _repository;
+        public UpdateCarCommandHandler(IRepository<Car> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(UpdateCarCommand command)
+        {
+            var value = await _repository.GetByIdAsync(command.CarId);
+
+            value.BigImageUrl = command.BigImageUrl;
+            value.BrandId = command.BrandId;
+            value.CoverImageUrl = command.CoverImageUrl;
+            value.Fuel = command.Fuel;
+            value.Luggage = command.Luggage;
+            value.Milage = command.Milage;
+            value.Model = command.Model;
+            value.Seat = command.Seat;
+            value.Transmission = command.Transmission;
+
+            await _repository.UpdateAsync(value);
+
+
+        }
+    }
+}
