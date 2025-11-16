dbba1d09ed7e193b84c90d35ab58dc207bacca08
Can <alikaya4275@gmail.com>
2025-11-16 17:23:05 +0300

    Footer Address Command ve Query'ler tamamlanması

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/FooterAddressHandlers/Write/CreateFooterAddressCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using CarBook.Application.Features.Mediator.Commands.FooterAddressCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.FooterAddressHandlers.Write
+{
+    public class CreateFooterAddressCommandHandler : IRequestHandler<CreateFooterAddressCommand>
+    {
+        private readonly IRepository<FooterAddres> _repository;
+
+        public CreateFooterAddressCommandHandler(IRepository<FooterAddres> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateFooterAddressCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new FooterAddres
+            {
+                Address = request.Address,
+                Description = request.Description,
+                Email = request.Email,
+                PhoneNumber = request.PhoneNumber,
+            });
+        }
+    }
+}
---
+++ Core/CarBook.Application/Features/Mediator/Handlers/FooterAddressHandlers/Write/UpdateFooterAddressCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Application.Features.Mediator.Commands.FooterAddressCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.FooterAddressHandlers.Write
+{
+    public class UpdateFooterAddressCommandHandler : IRequestHandler<UpdateFooterAddressCommand>
+    {
+        private readonly IRepository<FooterAddres> _repository;
+
+        public UpdateFooterAddressCommandHandler(IRepository<FooterAddres> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateFooterAddressCommand request, CancellationToken cancellationToken)
+        {
+
+            var value = await _repository.GetByIdAsync(request.FooterAddresId);
+            value.PhoneNumber = request.PhoneNumber;
+            value.Address = request.Address;
+            value.Description = request.Description;
+            value.Email = request.Email;
+        }
+    }
+}
