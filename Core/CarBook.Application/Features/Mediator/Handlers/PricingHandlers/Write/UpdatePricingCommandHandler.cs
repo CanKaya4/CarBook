dcecd20cf4803fb49e15355be387658831d19113
Can <alikaya4275@gmail.com>
2025-11-17 23:03:19 +0300

    ServiceCommand ve Service Api

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/PricingHandlers/Write/UpdatePricingCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Application.Features.Mediator.Commands.PricingCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+
+namespace CarBook.Application.Features.Mediator.Handlers.PricingHandlers.Write
+{
+    public class UpdatePricingCommandHandler : IRequestHandler<UpdatePricingCommand>
+    {
+        private readonly IRepository<Pricing> _repository;
+
+        public UpdatePricingCommandHandler(IRepository<Pricing> repository)
+        {
+            _repository = repository;
+        }
+
+
+        public async Task Handle(UpdatePricingCommand request, CancellationToken cancellationToken)
+        {
+
+            var value = await _repository.GetByIdAsync(request.PricingId);
+            value.PricingName = request.PricingName;
+
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
