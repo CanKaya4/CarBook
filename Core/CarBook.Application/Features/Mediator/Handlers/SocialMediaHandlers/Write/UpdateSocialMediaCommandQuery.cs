46796c94c29a4fc9f2479abc4b452383039166b5
Ali <alikaya4275@gmail.com>
2025-11-18 15:27:18 +0300

    Social Media Handler ve API

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/SocialMediaHandlers/Write/UpdateSocialMediaCommandQuery.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Application.Features.Mediator.Commands.SocialMediaCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.SocialMediaHandlers.Write
+{
+    public class UpdateSocialMediaCommandQuery : IRequestHandler<UpdateSocialMediaCommand>
+    {
+        private readonly IRepository<SocialMedia> _repository;
+
+        public UpdateSocialMediaCommandQuery(IRepository<SocialMedia> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateSocialMediaCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.SocialMediaId);
+            value.Url = request.Url;
+            value.Name = request.Name;
+            value.Icon = request.Icon;
+
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
